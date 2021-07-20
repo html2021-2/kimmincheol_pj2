b8ba5d9395e28d7cace8575209a0e459849c32f1
html2021-2 <html2021-2@daum.net>
2021-07-20 14:12:10 +0900

    탭 1080이하제어

--- js/tab.js
+++ js/tab.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
 	/* 1) 초기값 */
-  $('.tab:first-of-type, .tabpanel:first-of-type').addClass('active').attr('tabIndex', 0);
+  // $('.tab:first-of-type, .tabpanel:first-of-type').addClass('active').attr('tabIndex', 0);
+  // $('.tab:first-of-type').attr('aria-selected', true).siblings().attr('aria-selected', false);
+  // $('.tabpanel:first-of-type').attr('aria-hidden', false).siblings('.tabpanel').attr('aria-hidden', true);
+
+  $('#tab21, #tab31, #tabpanel21, #tabpanel31').addClass('active').attr('tabIndex', 0);
   $('.tab:first-of-type').attr('aria-selected', true).siblings().attr('aria-selected', false);
-  $('.tabpanel:first-of-type').attr('aria-hidden', false).siblings('.tabpanel').attr('aria-hidden', true);
+  $('#tabpanel21, #tabpanel31').attr('aria-hidden', false).siblings('.tabpanel').attr('aria-hidden', true);
 
   /* 2) 탭버튼에서 키보드를 누르는 이벤트(keydown) - 키보드 제어 */
   $('.tab').on('keydown', function (e) {
@@ -45,4 +49,12 @@ $(document).ready(function() {
     $target.addClass('active').attr({'aria-selected': true, tabIndex: 0}).siblings().removeClass('active').attr({'aria-selected': false, tabIndex: -1});
     $('#' + $target.attr('aria-controls')).addClass('active').attr({'aria-hidden': false, tabIndex: 0}).siblings('.tabpanel').removeClass('active').attr({'aria-hidden': true, tabIndex: -1});
   }
+
+  $(window).on('resize', function () {
+    const winWid = $(this).width();
+    const tabWrapUl = winWid * 2 * 0.8796;
+    console.log(tabWrapUl);
+    $('.product_tab ul').css({width: tabWrapUl});
+  });
+  $(window).trigger('resize');
 });
